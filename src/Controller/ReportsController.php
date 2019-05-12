3b7a500596982533979326e9c709e1b75504a5f8
Mark Jordan <mjordan@sfu.ca>
2019-05-12 17:14:49 +0000

    Work on #3.

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -9,16 +9,13 @@ use Drupal\Core\Controller\ControllerBase;
 */
 class ReportsController extends ControllerBase {
   /**
-   * Basic reports.
+   * Minimalist route for showing reports.
    *
    * @return string
    */
    public function reports() {
      return [
-       '#theme' => 'preservation_reports_chart',
-       '#chart' => TRUE,
-       '#pieheading' => "Sample chart showing relative percentages of Preservation Master MIME types",
-       '#lineheading' => "Sample chart showing fixity check event failures over time",
+       '#markup' => $this->t('Below are some reports.')
      ];
    }
 }
